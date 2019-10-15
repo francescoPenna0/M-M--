45cf7b2fd53ef3401e1b313d0f4e9a23a1e3d470
alfo93 <55742381+alfo93@users.noreply.github.com>
2019-10-15 23:40:42 +0200

    Add files via upload

--- funzioni.h
+++ funzioni.h
@@ -1,5 +1,25 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include <stdio.h>
 
+
+typedef struct{
+    double  tx; // tempo di servizio assegnato
+    double  tq; //tempo trascorso in coda
+    double  tk; //tempo trascorso nel sistema
+} Utente;
+
+typedef struct nodo{
+    Utente dato;
+    struct nodo* next;
+}Nodo;
+
+typedef Nodo* Lista;
+
+double poisson(double RISOLUZIONE,double lambda);
+void inizializzaSimulazione();
+void inserisciInCoda(Utente utente);
+Utente prelevaInTesta();
+void gestisciUscitaUtente(Utente utente, int *x, int *k,double *nUtUscitiServ, double *nUtUscitiSist);
+void gestisciServiUtenteInCoda(Utente *utente,int *x, int *q, double *nUtEntratiServ, double *nUtUscitiCoda);
+void simulazione();
+void stampaRisultati();
