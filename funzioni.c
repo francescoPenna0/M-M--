784ac3dee41ce1df432dee8267cced866404279a
francescoPenna0 <55707735+francescoPenna0@users.noreply.github.com>
2019-10-16 09:15:22 +0200

    enter

--- funzioni.c
+++ funzioni.c
@@ -4,5 +4,5 @@
 double poisson(double RISOLUZIONE,double lambda)
 // genera un tempo di interarrivo distribuito secondo Poisson
 {
-return (-logl(1 - (random((double)RISOLUZIONE) / (double)RISOLUZIONE) / (double)lambda)));
+return (-logl(1 - (random((double)RISOLUZIONE) / (double)RISOLUZIONE) / (double)lambda)))));
 }
